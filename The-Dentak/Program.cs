b372038271aaaff6618dfe29e4c3fdfa61cddff9
pecology <kobayashireo877@gmail.com>
2019-04-12 23:04:14 +0900

    足し算を実装

--- The-Dentak/Program.cs
+++ The-Dentak/Program.cs
@@ -6,7 +6,26 @@ namespace The_Dentak
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please input two number.");
+
+            Console.Write("num1:");
+            var firstInputString = Console.ReadLine();
+            if(!int.TryParse(firstInputString, out int num1))
+            {
+                Console.WriteLine($"{firstInputString} is not a number!!");
+                return;
+            }
+            
+            Console.Write("num2:");
+            var secondInputString = Console.ReadLine();
+            if(!int.TryParse(secondInputString, out int num2))
+            {
+                Console.WriteLine($"{secondInputString} is not a number!!");
+                return;
+            }
+
+            var sum = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {sum}");
         }
     }
 }
